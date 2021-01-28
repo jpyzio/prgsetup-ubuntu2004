814063a690ce176a0787e2766db7c83d0bb4f3e5
jpyzio <jakub@ittools.pl>
2021-01-28 11:03:58 +0100

    Docker-composer update 1.27.4 => 1.28.2

--- modules/24_docker-compose.sh
+++ modules/24_docker-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo curl --location "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
+sudo curl --location "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln --symbolic /usr/local/bin/docker-compose /usr/bin/docker-compose
