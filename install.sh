60abd28d242d3d4967fe14bf3d4fec3c717a62ca
Jakub Pyzio <jakub@ittools.pl>
2021-03-03 22:09:33 +0100

    Hot fixes

--- install.sh
+++ install.sh
@@ -31,67 +31,67 @@ sudo echo -e "\e[32mLet's start the installation ;)\e[39m"
 CHOICES=$(whiptail --checklist "Select which services do you want to install. " \
     30 77 22 \
     "0_system-update" "system update" on \
-    "1_kvm-for-android-studio.sh" "kvm for android studio" off \
-    "2_slack.sh" "slack" off \
-    "3_thunderbird.sh" "thunderbird" off \
-    "4_mysql.sh" "mysql" off \
-    "5_postgresql.sh" "postgresql" off \
-    "6_fiezilla.sh" "fiezilla" off \
-    "7_rsync.sh" "rsync" off \
-    "8_7zip.sh" "7zip" off \
-    "9_diff-utils.sh" "diff utils" off \
-    "10_insomnia.sh" "insomnia" off \
-    "11_postman.sh" "postman" off \
-    "12_nodejs-12.sh" "nodejs 12" off \
-    "13_yarn.sh" "yarn" off \
-    "14_php7.4-with-extensions.sh" "php7.4 with extensions" off \
-    "15_composer.sh" "composer" off \
-    "16_composer-test-utils.sh" "composer test utils" off \
-    "17_sumfony-cli.sh" "sumfony cli" off \
-    "18_diagnostic-tools.sh" "diagnostic tools" off \
-    "19_network-tools.sh" "network tools" off \
-    "20_gparted.sh" "gparted" off \
-    "21_smart-tools.sh" "smart tools" off \
-    "22_secure-delete.sh" "secure delete" off \
-    "23_docker.sh" "docker" off \
-    "24_docker-compose.sh" "docker compose" off \
-    "25_git.sh" "git" off \
-    "26_git-hooks.sh" "git hooks" off \
-    "27_git-config.sh" "git config" off \
-    "28_gpg.sh" "gpg" off \
-    "29_gpg-create-key.sh" "gpg create key" off \
-    "30_gimp.sh" "gimp" off \
-    "31_webp.sh" "webp" off \
-    "32_nautilus-extensions.sh" "nautilus extensions" off \
-    "33_sublime-text-3.sh" "sublime text 3" off \
-    "34_jetbrains-toolbox.sh" "jetbrains toolbox" off \
-    "35_jakoob-system-dock.sh" "jakoob system dock" off \
-    "36_jakoob-aliases.sh" "jakoob aliases" off \
-    "37_shellcheck.sh" "shellcheck" off \
-    "38_speedtest.sh" "speedtest" off \
-    "39_cpufreq.sh" "cpufreq" off \
-    "40_cpufreq-set-performance.sh" "cpufreq set performance" off \
-    "41_jakub-user-groups.sh" "jakub user groups" off \
-    "42_vlc.sh" "vlc" off \
-    "43_spotify.sh" "spotify" off \
-    "44_libreoffice.sh" "libreoffice" off \
-    "45_ufw.sh" "ufw" off \
-    "46_rkhunter.sh" "rkhunter" off \
-    "47_ssh-keygen.sh" "ssh keygen" off \
-    "48_ssh-server.sh" "ssh server" off \
-    "49_sshfs.sh" "sshfs" off \
-    "50_nfs.sh" "nfs" off \
-    "51_ftpfs.sh" "ftpfs" off \
-    "52_openvpn-client.sh" "openvpn client" off \
-    "53_zsh.sh" "zsh" off \
-    "54_tmux.sh" "tmux" off \
-    "55_oh-my-zsh.sh" "oh my zsh" off \
-    "56_zsh-fzf.sh" "zsh fzf" off \
-    "57_jakoob-zsh-tuning.sh" "jakoob zsh tuning" off \
-    "58_virtualbox.sh" "virtualbox" off \
-    "59_chrome.sh" "chrome" off \
-    "60_firefox.sh" "firefox" off \
-    "61_obs-studio.sh" "OBS Studio" off \
+    "1_kvm-for-android-studio" "kvm for android studio" on \
+    "2_slack" "slack" on \
+    "3_thunderbird" "thunderbird" on \
+    "4_mysql" "mysql" on \
+    "5_postgresql" "postgresql" on \
+    "6_fiezilla" "fiezilla" on \
+    "7_rsync" "rsync" on \
+    "8_7zip" "7zip" on \
+    "9_diff-utils" "diff utils" on \
+    "10_insomnia" "insomnia" on \
+    "11_postman" "postman" on \
+    "12_nodejs-12" "nodejs 12" on \
+    "13_yarn" "yarn" on \
+    "14_php7.4-with-extensions" "php7.4 with extensions" on \
+    "15_composer" "composer" on \
+    "16_composer-test-utils" "composer test utils" on \
+    "17_symfony-cli" "symfony cli" on \
+    "18_diagnostic-tools" "diagnostic tools" on \
+    "19_network-tools" "network tools" on \
+    "20_gparted" "gparted" on \
+    "21_smart-tools" "smart tools" on \
+    "22_secure-delete" "secure delete" on \
+    "23_docker" "docker" on \
+    "24_docker-compose" "docker compose" on \
+    "25_git" "git" on \
+    "26_git-hooks" "git hooks" on \
+    "27_git-config" "git config" on \
+    "28_gpg" "gpg" on \
+    "29_gpg-create-key" "gpg create key" on \
+    "30_gimp" "gimp" on \
+    "31_webp" "webp" on \
+    "32_nautilus-extensions" "nautilus extensions" on \
+    "33_sublime-text-3" "sublime text 3" on \
+    "34_jetbrains-toolbox" "jetbrains toolbox" on \
+    "35_jakoob-system-dock" "jakoob system dock" on \
+    "36_jakoob-aliases" "jakoob aliases" on \
+    "37_shellcheck" "shellcheck" on \
+    "38_speedtest" "speedtest" on \
+    "39_cpufreq" "cpufreq" on \
+    "40_cpufreq-set-performance" "cpufreq set performance" on \
+    "41_jakub-user-groups" "jakub user groups" on \
+    "42_vlc" "vlc" on \
+    "43_spotify" "spotify" on \
+    "44_libreonice" "libreonice" on \
+    "45_ufw" "ufw" on \
+    "46_rkhunter" "rkhunter" on \
+    "47_ssh-keygen" "ssh keygen" on \
+    "48_ssh-server" "ssh server" on \
+    "49_sshfs" "sshfs" on \
+    "50_nfs" "nfs" on \
+    "51_ftpfs" "ftpfs" on \
+    "52_openvpn-client" "openvpn client" on \
+    "53_zsh" "zsh" on \
+    "54_tmux" "tmux" on \
+    "55_oh-my-zsh" "oh my zsh" on \
+    "56_zsh-fzf" "zsh fzf" on \
+    "57_jakoob-zsh-tuning" "jakoob zsh tuning" on \
+    "58_virtualbox" "virtualbox" on \
+    "59_chrome" "chrome" on \
+    "60_firefox" "firefox" on \
+    "61_obs-studio" "OBS Studio" on \
     3>&2 2>&1 1>&3)
 
 if [[ "${CHOICES}" == "" ]]; then
