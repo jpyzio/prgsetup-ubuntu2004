1fb9426929a2b22f27e8661ea68ab5257e901653
jpyzio <jakub@ittools.pl>
2021-03-12 22:29:00 +0100

    Allow the user to access USB devices

--- modules/41_jakub-user-groups.sh
+++ modules/41_jakub-user-groups.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for GROUP in video kvm www-data plugdev sambashare lpadmin adm sudo ; do
+for GROUP in video kvm www-data plugdev sambashare lpadmin adm sudo dialout ; do
   groups | grep "${GROUP}" --quiet
   if [[ "${?}" == "1" ]]; then # The user does not belong to the group
     cut -d: -f1 /etc/group | tr '\n' ' ' | grep "${GROUP}" --quiet
