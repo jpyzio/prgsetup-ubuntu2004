bf20245ecb21c7a07de5f973c8b5a306a7b82ed7
Jakub Pyzio <jakub@ittools.pl>
2021-03-28 21:08:59 +0200

    #3 Add first level instalation menu

--- install.sh
+++ install.sh
@@ -26,83 +26,103 @@ if [[ "${UBUNTU_VERSION}" != "${CONFIGURATOR_VERSION}" ]]; then
 fi
 ### END System checker
 
-sudo echo -e "\e[32mLet's start the installation ;)\e[39m"
-
-CHOICES=$(whiptail --checklist "Select which services do you want to install. " \
-    30 77 22 \
-    "system-update" "system update" on \
-    "kvm-for-android-studio" "kvm for android studio" on \
-    "slack" "slack" on \
-    "thunderbird" "thunderbird" on \
-    "mysql" "mysql" on \
-    "postgresql" "postgresql" on \
-    "fiezilla" "fiezilla" on \
-    "rsync" "rsync" on \
-    "7zip" "7zip" on \
-    "diff-utils" "diff utils" on \
-    "insomnia" "insomnia" on \
-    "postman" "postman" on \
-    "nodejs-12" "nodejs 12" on \
-    "yarn" "yarn" on \
-    "php7.4-with-extensions" "php7.4 with extensions" on \
-    "composer" "composer" on \
-    "composer-test-utils" "composer test utils" on \
-    "symfony-cli" "symfony cli" on \
-    "diagnostic-tools" "diagnostic tools" on \
-    "network-tools" "network tools" on \
-    "gparted" "gparted" on \
-    "smart-tools" "smart tools" on \
-    "secure-delete" "secure delete" on \
-    "docker" "docker" on \
-    "docker-compose" "docker compose" on \
-    "git" "git" on \
-    "git-hooks" "git hooks" on \
-    "git-config" "git config" on \
-    "gpg" "gpg" on \
-    "gpg-create-key" "gpg create key" on \
-    "gimp" "gimp" on \
-    "webp" "webp" on \
-    "nautilus-extensions" "nautilus extensions" on \
-    "sublime-text-3" "sublime text 3" on \
-    "jetbrains-toolbox" "jetbrains toolbox" on \
-    "jakoob-system-dock" "jakoob system dock" on \
-    "jakoob-aliases" "jakoob aliases" on \
-    "shellcheck" "shellcheck" on \
-    "speedtest" "speedtest" on \
-    "cpufreq" "cpufreq" on \
-    "cpufreq-set-performance" "cpufreq set performance" on \
-    "jakub-user-groups" "jakub user groups" on \
-    "vlc" "vlc" on \
-    "spotify" "spotify" on \
-    "libreonice" "libreonice" on \
-    "ufw" "ufw" on \
-    "rkhunter" "rkhunter" on \
-    "ssh-keygen" "ssh keygen" on \
-    "ssh-server" "ssh server" on \
-    "sshfs" "sshfs" on \
-    "nfs" "nfs" on \
-    "ftpfs" "ftpfs" on \
-    "openvpn-client" "openvpn client" on \
-    "zsh" "zsh" on \
-    "tmux" "tmux" on \
-    "oh-my-zsh" "oh my zsh" on \
-    "zsh-fzf" "zsh fzf" on \
-    "jakoob-zsh-tuning" "jakoob zsh tuning" on \
-    "virtualbox" "virtualbox" on \
-    "chrome" "chrome" on \
-    "firefox" "firefox" on \
-    "obs-studio" "OBS Studio" on \
+INSTALATION_PROFILE=$(whiptail --radiolist "Select which services do you want to install. " \
+    10 52 5 \
+    "full" "All packages" on \
+    "mini" "Minimal installation" off \
+    "custom" "Choose your favourite packages" off \
     3>&2 2>&1 1>&3)
 
+
+if [[ "${INSTALATION_PROFILE}" == "full" ]]; then
+    CHOICES="system-update kvm-for-android-studio slack thunderbird mysql postgresql fiezilla rsync 7zip diff-utils insomnia postman nodejs-12 yarn php7.4-with-extensions" \
+    " composer composer-test-utils symfony-cli diagnostic-tools network-tools gparted smart-tools secure-delete docker docker-compose git git-hooks git-config gpg gpg-create-key" \
+    " gimp webp nautilus-extensions sublime-text-3 jetbrains-toolbox jakoob-system-dock jakoob-aliases shellcheck speedtest cpufreq cpufreq-set-performance jakub-user-groups vlc" \
+    " spotify libreonice ufw rkhunter ssh-keygen ssh-server sshfs nfs ftpfs openvpn-client zsh tmux oh-my-zsh zsh-fzf jakoob-zsh-tuning virtualbox chrome firefox obs-studio"
+fi
+
+if [[ "${INSTALATION_PROFILE}" == "mini" ]]; then
+    CHOICES="system-update zsh chrome thunderbird git sublime-text-3 ufw"
+fi
+
+if [[ "${INSTALATION_PROFILE}" == "custom" ]]; then
+    CHOICES=$(whiptail --checklist "Select which services do you want to install. " \
+        30 77 22 \
+        "system-update" "system update" off \
+        "kvm-for-android-studio" "kvm for android studio" off \
+        "slack" "slack" off \
+        "thunderbird" "thunderbird" off \
+        "mysql" "mysql" off \
+        "postgresql" "postgresql" off \
+        "fiezilla" "fiezilla" off \
+        "rsync" "rsync" off \
+        "7zip" "7zip" off \
+        "diff-utils" "diff utils" off \
+        "insomnia" "insomnia" off \
+        "postman" "postman" off \
+        "nodejs-12" "nodejs 12" off \
+        "yarn" "yarn" off \
+        "php7.4-with-extensions" "php7.4 with extensions" off \
+        "composer" "composer" off \
+        "composer-test-utils" "composer test utils" off \
+        "symfony-cli" "symfony cli" off \
+        "diagnostic-tools" "diagnostic tools" off \
+        "network-tools" "network tools" off \
+        "gparted" "gparted" off \
+        "smart-tools" "smart tools" off \
+        "secure-delete" "secure delete" off \
+        "docker" "docker" off \
+        "docker-compose" "docker compose" off \
+        "git" "git" off \
+        "git-hooks" "git hooks" off \
+        "git-config" "git config" off \
+        "gpg" "gpg" off \
+        "gpg-create-key" "gpg create key" off \
+        "gimp" "gimp" off \
+        "webp" "webp" off \
+        "nautilus-extensions" "nautilus extensions" off \
+        "sublime-text-3" "sublime text 3" off \
+        "jetbrains-toolbox" "jetbrains toolbox" off \
+        "jakoob-system-dock" "jakoob system dock" off \
+        "jakoob-aliases" "jakoob aliases" off \
+        "shellcheck" "shellcheck" off \
+        "speedtest" "speedtest" off \
+        "cpufreq" "cpufreq" off \
+        "cpufreq-set-performance" "cpufreq set performance" off \
+        "jakub-user-groups" "jakub user groups" off \
+        "vlc" "vlc" off \
+        "spotify" "spotify" off \
+        "libreonice" "libreonice" off \
+        "ufw" "ufw" off \
+        "rkhunter" "rkhunter" off \
+        "ssh-keygen" "ssh keygen" off \
+        "ssh-server" "ssh server" off \
+        "sshfs" "sshfs" off \
+        "nfs" "nfs" off \
+        "ftpfs" "ftpfs" off \
+        "openvpn-client" "openvpn client" off \
+        "zsh" "zsh" off \
+        "tmux" "tmux" off \
+        "oh-my-zsh" "oh my zsh" off \
+        "zsh-fzf" "zsh fzf" off \
+        "jakoob-zsh-tuning" "jakoob zsh tuning" off \
+        "virtualbox" "virtualbox" off \
+        "chrome" "chrome" off \
+        "firefox" "firefox" off \
+        "obs-studio" "OBS Studio" off \
+    3>&2 2>&1 1>&3)
+fi
+echo "${CHOICES}"; exit;
 if [[ "${CHOICES}" == "" ]]; then
-    exit 0
+    echp "No packages selected"
+    exit 1
 fi
 
 sudo apt update
 
 if [[ ! -f "${ROOT_DIR}/.installed" ]] ; then
     # shellcheck disable=SC1090
-    source "${MODULES_DIR}/0_system-update.sh"
+    source "${MODULES_DIR}/system-update.sh"
     touch "${ROOT_DIR}/.installed"
 fi
 
