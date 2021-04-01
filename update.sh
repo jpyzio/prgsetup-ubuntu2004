2194c53ec913f7d98ec921f5845095f918202c95
Jakub Pyzio <jakub@ittools.pl>
2021-04-01 22:54:38 +0200

    Fix spaces

--- update.sh
+++ update.sh
@@ -12,8 +12,8 @@ LAST_UPDATE_FILE="${ROOT_DIR}/.last_update"
 CURRENT_TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
 if [[ ! -f "${LAST_UPDATE_FILE}" ]]; then
-	echo "${MODULES_DIR}/update.sh" | tee --append ~/.zshrc ~/.bashrc
-	exit
+    echo "${MODULES_DIR}/update.sh" | tee --append ~/.zshrc ~/.bashrc
+    exit
 fi
 
 LAST_UPDATE_TIME=$(date -d "$(cat ${LAST_UPDATE_FILE})" +%s)
@@ -21,13 +21,12 @@ CURRENT_TIME=$(date -d "${CURRENT_TIMESTAMP}" +%s)
 SEC_SINCE_UPDATE=$((CURRENT_TIME-LAST_UPDATE_TIME))
 
 if (( "${SEC_SINCE_UPDATE}" < "${UPDATE_DAYS}" )); then
-	exit
+    exit
 fi
 
 read -p "Do you want to update your system and other tools? [y/N]" -n 1 -r
 echo
 if [[ $REPLY =~ ^[YyTt]$ ]]; then
     source "${MODULES_DIR}/system-update.sh"
-
-echo "${CURRENT_TIMESTAMP}" > "${LAST_UPDATE_FILE}"
+    echo "${CURRENT_TIMESTAMP}" > "${LAST_UPDATE_FILE}"
 fi
