af178126d53f0afd6b24d06c1f064eaa0b132640
Jakub Pyzio <jakub@ittools.pl>
2021-04-03 00:57:31 +0200

    Fetch newest docker-compose version

--- modules/docker-compose.sh
+++ modules/docker-compose.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo curl --location "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
+LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f4)
+
+sudo curl --location "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln --symbolic /usr/local/bin/docker-compose /usr/bin/docker-compose
