6d4fec08297310a1759fd061de992849bb013e6b
jpyzio <jakub@ittools.pl>
2021-04-03 00:34:36 +0200

    Add dependencies for composer package (#14)
    * Add dependencies for composer package
    
    * Add phpunit package

--- modules/composer-test-utils.sh
+++ modules/composer-test-utils.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-composer global require  sebastian/phpcpd phploc/phploc phpmd/phpmd squizlabs/php_codesniffer phpstan/phpstan
+which composer > /dev/null
+if [[ "${?}" == "1" ]]; then
+	source "${MODULES_DIR}/composer.sh"
+fi
+
+composer global require \
+    sebastian/phpcpd \
+    phploc/phploc \
+    phpmd/phpmd \
+    squizlabs/php_codesniffer \
+    phpstan/phpstan \
+    phpunit/phpunit
--- modules/composer.sh
+++ modules/composer.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+which php > /dev/null
+if [[ "${?}" == "1" ]]; then
+	source "${MODULES_DIR}/php7.4-with-extensions.sh"
+fi
+
 curl --silent --show-error https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
