c53dab5fc643ce13ef80c972195b68817a0c0d79
Jakub Pyzio <jakub@ittools.pl>
2021-04-02 14:01:19 +0200

    Virtualbox fixes

--- modules/virtualbox.sh
+++ modules/virtualbox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install --yes virtualbox virtualbox-ext-pack
+sudo apt install --yes virtualbox virtualbox-ext-pack virtualbox-dkms
 
 groups | grep vboxusers --quiet
 if [[ ${?} == "1" ]]; then
