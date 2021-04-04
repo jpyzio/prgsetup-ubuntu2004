0b002d4e6c2c4920e7ece12475f95a7c20ffba3b
Jakub Pyzio <jakub@ittools.pl>
2021-04-05 01:34:51 +0200

    Change 2 spaces to 4

--- modules/jakoob-aliases.sh
+++ modules/jakoob-aliases.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for FILE in ~/.bashrc ~/.zshrc; do
-  grep -E "source.*custom-functions.sh" "${FILE}" --quiet
-  if [[ "${?}" == "1" ]]; then
-    echo "source \"${ROOT_DIR}/aliases.sh\"" | tee --append "${FILE}"
-  fi
+    grep -E "source.*custom-functions.sh" "${FILE}" --quiet
+    if [[ "${?}" == "1" ]]; then
+        echo "source \"${ROOT_DIR}/aliases.sh\"" | tee --append "${FILE}"
+    fi
 done
