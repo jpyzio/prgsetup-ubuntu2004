d7c9ae92473fb498b3a5151a59f43fa05d3af0f0
Jakub Pyzio <jakub@ittools.pl>
2021-04-06 01:02:41 +0200

    Cleanup

--- modules/docker.sh
+++ modules/docker.sh
@@ -2,8 +2,6 @@
 
 sudo apt purge --yes docker docker.io containerd runc
 
-sudo apt-get install --yes apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-
 curl --fail --silent --show-error --location https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
