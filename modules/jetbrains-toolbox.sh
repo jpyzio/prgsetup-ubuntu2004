679ec3a8af8c6cca8ca965c7349af7584bdd2e9d
Jakub Pyzio <jakub@ittools.pl>
2021-04-07 02:53:10 +0200

    Fix spelling

--- modules/jetbrains-toolbox.sh
+++ modules/jetbrains-toolbox.sh
@@ -2,17 +2,17 @@
 
 if [[ ! -f ~/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ]]; then
     RELEASE_JSON=$(curl --silent "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release")
-    LASTEST_URL=$(printf %s "${RELEASE_JSON}" | jq '.TBA[0].downloads.linux.link' | tr --delete '"')
-    LASTEST_BUILD=$(printf %s "${RELEASE_JSON}" | jq '.TBA[0].build' | tr --delete '"')
+    LATEST_URL=$(printf %s "${RELEASE_JSON}" | jq '.TBA[0].downloads.linux.link' | tr --delete '"')
+    LATEST_BUILD=$(printf %s "${RELEASE_JSON}" | jq '.TBA[0].build' | tr --delete '"')
     TOOLBOX_FILE="${ROOT_DIR}/toolbox.tgz"
 
-    wget --output-document="${TOOLBOX_FILE}" "${LASTEST_URL}"
+    wget --output-document="${TOOLBOX_FILE}" "${LATEST_URL}"
     tar --extract --gzip --file="${TOOLBOX_FILE}"
 
-    jetbrains-toolbox-"${LASTEST_BUILD}"/jetbrains-toolbox
+    jetbrains-toolbox-"${LATEST_BUILD}"/jetbrains-toolbox
 
     rm "${TOOLBOX_FILE}"
-    rm --recursive --force jetbrains-toolbox-"${LASTEST_BUILD}"
+    rm --recursive --force jetbrains-toolbox-"${LATEST_BUILD}"
 fi
 
 grep 'max_user_watches' /etc/sysctl.conf
