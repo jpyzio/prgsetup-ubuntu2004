df73f812bc17726febad14b08cf4f636e10253ed
Jakub Pyzio <jakub@ittools.pl>
2021-04-09 04:48:49 +0200

    Refactor

--- modules/jakub-user-groups.sh
+++ modules/jakub-user-groups.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 for GROUP in video kvm www-data plugdev sambashare lpadmin adm sudo dialout ; do
-    groups | grep --quiet "${GROUP}"
-    if [[ "${?}" == "1" ]]; then # The user does not belong to the group
+    if ! groups | grep --quiet "${GROUP}"; then # The user does not belong to the group
         if cut -d: -f1 /etc/group | tr '\n' ' ' | grep --quiet "${GROUP}"; then # The group exists
             sudo usermod --append --groups "${GROUP}" "${USER}"
         fi
