9a39d41488ae0c6d0a968ed5e688fbf31e05250c
Jakub Pyzio <jakub@ittools.pl>
2021-04-09 03:59:11 +0200

    Refactor

--- modules/jakoob-aliases.sh
+++ modules/jakoob-aliases.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 for FILE in ~/.bashrc ~/.zshrc; do
-    grep -E "source.*custom-functions.sh" "${FILE}" --quiet
-    if [[ "${?}" == "1" ]]; then
+    if ! grep -E "source.*aliases.sh" "${FILE}" --quiet; then
         echo "source \"${ROOT_DIR}/aliases.sh\"" | tee --append "${FILE}"
     fi
 done
