39cc563fc26f125f13def3e2a3a422cce6bd704b
Jakub Pyzio <jakub@ittools.pl>
2021-05-03 03:53:28 +0200

    Fix small bugs #45

--- modules/brave.sh
+++ modules/brave.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-GPG_KEY_PATH=/usr/share/keyrings/brave-browser-archive-keyring.gpg
-GPG_KEY_URL=https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+if ! which brave-browser > /dev/null; then
+  GPG_KEY_PATH=/usr/share/keyrings/brave-browser-archive-keyring.gpg
+  GPG_KEY_URL=https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-curl -fsSLo "${GPG_KEY_PATH}" "${GPG_KEY_URL}"
+  curl -fsSLo "${GPG_KEY_PATH}" "${GPG_KEY_URL}"
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
-    > /etc/apt/sources.list.d/brave-browser-release.list
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+      > /etc/apt/sources.list.d/brave-browser-release.list
 
-apt update
-apt install brave-browser
+  apt update
+
+  apt install brave-browser
+fi
--- modules/docker-compose.sh
+++ modules/docker-compose.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | cut -d'v' -f2 | cut -d'"' -f4)
+BIN_FILE_URL="https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname --kernel-name)-$(uname --machine)"
 
-curl --location "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
+curl --location --silent "${BIN_FILE_URL}" --output /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 ln --symbolic --force /usr/local/bin/docker-compose /usr/bin/docker-compose
