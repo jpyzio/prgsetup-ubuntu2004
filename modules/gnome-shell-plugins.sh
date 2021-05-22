5304dfd3c2d20bed9cb221c9d9aea1cd3f9088c7
Jakub Pyzio <jakub@ittools.pl>
2021-05-23 01:42:47 +0200

    Add gnome shell plugins

--- modules/gnome-shell-plugins.sh
+++ modules/gnome-shell-plugins.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-# https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
-# https://extensions.gnome.org/extension/750/openweather/
+run_as_user google-chrome https://chrome.google.com/webstore/detail/gnome-shell-integration/gphhapmejobijbbhgpjhcjognlahblep/
+
+run_as_user google-chrome https://extensions.gnome.org/extension/1271/sound-settings/ \
+    https://extensions.gnome.org/extension/7/removable-drive-menu/ \
+    https://extensions.gnome.org/extension/750/openweather/ \
+    https://extensions.gnome.org/extension/104/netspeed/ \
+    https://extensions.gnome.org/extension/1465/desktop-icons/ \
+    https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/ \
+    https://extensions.gnome.org/extension/945/cpu-power-manager/
