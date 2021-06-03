0ab0b584189d119b867db654df3835113c6b1ce9
Jakub Pyzio <jakub@ittools.pl>
2021-06-03 18:42:43 +0200

    Feature/extract jakub customs (#65)
    * Extract Jakub customs module to external script
    
    * Chmod +x jskub-customs

---
+++ common.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -o xtrace
+
+SHOULD_REBOOT=false
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MODULES_DIR="${ROOT_DIR}/modules"
+USER_NAME="$(logname)"
+USER_HOME="$(eval echo ~"${USER_NAME}")"
+
+run_as_user() {
+    sudo -i -H -u "${USER_NAME}" "${@}"
+}
+
+text_input() {
+    zenity --entry --title="Ubuntu Configurator" --text="${1}"
+}
+
+password_input() {
+    zenity --password --title="Ubuntu Configurator" --text="${1}"
+}
+
+function is_installed() {
+    if [[ "${INSTALATION_PROFILE}" == "custom-zero" ]]; then
+        echo "off"
+        return
+    fi
+
+    if [[ ! -f "${ROOT_DIR}/.installed_modules" ]]; then
+        echo "on"
+        return
+    fi
+
+
+    if grep --quiet "${1}" "${ROOT_DIR}/.installed_modules"; then
+        echo "off"
+    else
+        echo "on"
+    fi
+}
+
+### BEGIN System checker
+CONFIGURATOR_VERSION="20.04"
+UBUNTU_VERSION=$(lsb_release --release --short)
+
+if [[ "${UBUNTU_VERSION}" != "${CONFIGURATOR_VERSION}" ]]; then
+    echo -e "\e[31mERROR: This configurator is only for Ubuntu ${CONFIGURATOR_VERSION}\e[39m"
+    exit 1
+fi
+### END System checker
