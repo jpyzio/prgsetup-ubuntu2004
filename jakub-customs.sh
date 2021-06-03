46a5445ee49bccde8dfd5b88d7540d0ed157c666
Jakub Pyzio <jakub.pyzio@farmaprom.pl>
2021-06-03 20:08:55 +0200

    Remove desktop-icons plugin installation

--- jakub-customs.sh
+++ jakub-customs.sh
@@ -55,7 +55,6 @@ if which google-chrome > /dev/null; then
         https://extensions.gnome.org/extension/7/removable-drive-menu/ \
         https://extensions.gnome.org/extension/750/openweather/ \
         https://extensions.gnome.org/extension/104/netspeed/ \
-        https://extensions.gnome.org/extension/1465/desktop-icons/ \
         https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/ \
         https://extensions.gnome.org/extension/945/cpu-power-manager/ \
         https://extensions.gnome.org/extension/779/clipboard-indicator/
