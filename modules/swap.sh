5dbde9b0beb05e24e609bf8c8c88ea98f971be38
Jakub Pyzio <jakub@ittools.pl>
2021-06-29 16:12:47 +0200

    Add SWAP file (#78)

--- modules/swap.sh
+++ modules/swap.sh
@@ -1,15 +1,29 @@
-#!/usr/bin/env bash
+set -e
+
+SWAP_FILE='/swapfile'
+FSTAB_FILE='/etc/fstab'
+
+if swapon -s | grep -q "${SWAP_FILE}"; then
+    swapoff "${SWAP_FILE}"
+fi
+
+if grep "${SWAP_FILE}" "${FSTAB_FILE}"; then
+    sed -i "/^\\${SWAP_FILE}.*/d" "${FSTAB_FILE}"
+fi
+
+if [[ -f "${SWAP_FILE}" ]]; then
+    rm "${SWAP_FILE}"
+fi
 
 while ! echo "${SWAP_SIZE}" | grep -qE '^[0-9]+$'; do
-    SWAP_SIZE=$(text_input "SWAP size in GB (only number)")
+    SWAP_SIZE=$(text_input "SWAP size in GB (0 = no SWAP)")
 done
 
-fallocate -l "${SWAP_SIZE}G" /swapfile
+if [[ "${SWAP_SIZE}" -gt 0 ]]; then
+    fallocate -l "${SWAP_SIZE}G" "${SWAP_FILE}"
+    chmod 600 "${SWAP_FILE}"
+    mkswap "${SWAP_FILE}"
+    swapon "${SWAP_FILE}"
 
-chmod 600 /swapfile
-
-mkswap /swapfile
-
-swapon /swapfile
-
-echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
+    echo "${SWAP_FILE} swap swap defaults 0 0" >> "${FSTAB_FILE}"
+fi
